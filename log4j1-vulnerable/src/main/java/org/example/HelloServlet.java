5b6125e0c0d0f44876cc54b8c4a7bbf981e6f990
Shane Schisler <shane.schisler@contrastsecurity.com>
2021-12-20 14:34:12 -0500

    add log4j1 vulnerable app.

---
+++ log4j1-vulnerable/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/world")
+public class HelloServlet extends HttpServlet {
+
+  private static Logger logger = Logger.getRootLogger();
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    var parm1 = req.getParameter("foo");
+    var parm2 = req.getParameter("bar");
+
+    logger.info("parm1 is " + parm1);  // log-injection
+    var a = StringUtils.chop(parm1);
+
+    var f = new File(a);
+    try (var fos = new FileOutputStream(f)) {
+      fos.write("Hello World".getBytes(StandardCharsets.UTF_8));
+    }
+  }
+
+  private String myfunc1(String a) {
+    return a;
+  }
+
+  private String myfunc2(String a, String b) {
+    return a + b;
+  }
+
+  private String myfunc3(String a) {
+    return a.toLowerCase();
+  }
+}
